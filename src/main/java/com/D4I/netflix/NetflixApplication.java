e8b2da3f3026e794783d368eaf68ea603cf51c7c
aliciaOrellana <alicia.maria.orellana.cocinero@everis.nttdata.com>
2021-06-14 13:54:40 +0200

    Primer commit
    Primeros ficheros para empezar a trabajar

---
+++ src/main/java/com/D4I/netflix/NetflixApplication.java
@@ -0,0 +1,13 @@
+package com.D4I.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Netflix1Application.class, args);
+	}
+
+}
